fd3f04b5520f72d5d26078867479abe8751f6066
Bowen Tao <bowentao2015@gmail.com>
2023-06-05 18:22:56 +0800

    Update Fragment.hlsl

--- Assets/CustomRP/ShaderLibrary/Fragment.hlsl
+++ Assets/CustomRP/ShaderLibrary/Fragment.hlsl
@@ -18,7 +18,7 @@ Fragment GetFragment(float4 positionSS)
 {
     Fragment f;
     f.positionSS = positionSS.xy;
-    f.screenUV = f.positionSS / _CameraBufferSize.xy;
+    f.screenUV = f.positionSS * _CameraBufferSize.xy;
     f.depth = IsOrthographicCamera() ? OrthographicDepthBufferToLinear(positionSS.z) : positionSS.w;
     f.bufferDepth = SAMPLE_DEPTH_TEXTURE_LOD(_CameraDepthTexture, sampler_point_clamp, f.screenUV, 0);
     f.bufferDepth = IsOrthographicCamera() ? OrthographicDepthBufferToLinear(f.bufferDepth) : LinearEyeDepth(f.bufferDepth, _ZBufferParams);
